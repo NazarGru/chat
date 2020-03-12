3f4e7ee0416390667359770995d39d99c1afbe01
NazarGru <nazarogarishev333@gmail.com>
2020-03-12 09:09:09 +0200

    bild

---
+++ chat/build/precache-manifest.c5131670d03ae0c5abef245ba860e416.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1e9cea321c7ba824d2be2e21340f659",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "39163f2e15d1c18da3d6",
+    "url": "/chat/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2bca8e2b64d9f9d97ee3",
+    "url": "/chat/static/js/2.a8388fc9.chunk.js"
+  },
+  {
+    "revision": "54aadce522968e9a553318ceafe9adc6",
+    "url": "/chat/static/js/2.a8388fc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39163f2e15d1c18da3d6",
+    "url": "/chat/static/js/main.5505f30a.chunk.js"
+  },
+  {
+    "revision": "b29dd95284439478bcd9",
+    "url": "/chat/static/js/runtime-main.6085059e.js"
+  }
+]);
